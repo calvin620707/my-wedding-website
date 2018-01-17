a2206b7fd1d6e0a499aa8d0ef93885242d158ded
Calvin Wu <calvin620707@msn.com>
2018-01-17 22:20:34 +0800

    next with ant design

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,14 @@
-import React from 'react'
-
-class Layout extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-                <h1>Body</h1>
-            </div>
-        )
-    }
-}
-
-export default Layout
+import Head from 'next/head'
+export default ({ children }) =>
+    <div>
+        <Head>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <meta charSet='utf-8' />
+            <link rel='stylesheet' href='https://unpkg.com/antd@3/dist/antd.min.css' />
+        </Head>
+        <style jsx global>{`
+      body {
+      }
+    `}</style>
+        {children}
+    </div>
